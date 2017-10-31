028b62861dce59054b20b012cbb0878dc0ef5311
maria <maria@example.com>
2017-10-31 19:16:43 +0100

    First commit

---
+++ src/main/java/org/accenture/fac/Factorial.java
@@ -0,0 +1,17 @@
+package org.accenture.fac;
+
+public class Factorial {
+
+	public long compute(long value) {
+		if((value==0)||(value==1)){
+			return 1;
+		}else if(value==2) {
+			return 2;
+		}else if(value==3) {
+			return 6;
+		}else {
+			return value*compute(value-1);			
+		}
+	}
+	
+}
