0dee9d02a6dfb6e3bacfe8290a824a0f830aac21
maria <maria@example.com>
2017-10-31 19:41:14 +0100

    Second commit

--- src/test/java/org/accenture/fac/FactorialTest.java
+++ src/test/java/org/accenture/fac/FactorialTest.java
@@ -2,47 +2,53 @@ package org.accenture.fac;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FactorialTest {
-	@Test
-	public void Test1() {
-		Factorial factorial= new Factorial();
-		
-		long result= factorial.compute(0);
-		long expectedValue=1;
-		
-		assertEquals(expectedValue,result);
-		
+	private Factorial factorial;
+
+	@Before
+	public void init() {
+		factorial = new Factorial();
 	}
+
 	@Test
-	public void Test2() {
-		Factorial factorial= new Factorial();
-		
-		long result= factorial.compute(1);
-		long expectedValue=1;
-		
-		assertEquals(expectedValue,result);
-		
+	public void shouldFactorialOfZeroReturnOne() {
+
+		long result = factorial.compute(0);
+		long expectedValue = 1;
+
+		assertEquals(expectedValue, result);
+
 	}
+
 	@Test
-	public void Test3() {
-		Factorial factorial= new Factorial();
-		
-		long result= factorial.compute(2);
-		long expectedValue=2;
-		
-		assertEquals(expectedValue,result);
-		
+	public void shouldFactorialOfOneReturnOne() {
+
+		long result = factorial.compute(1);
+		long expectedValue = 1;
+
+		assertEquals(expectedValue, result);
+
 	}
+
 	@Test
-	public void Test4() {
-		Factorial factorial= new Factorial();
-		
-		long result= factorial.compute(5);
-		long expectedValue=120;
-		
-		assertEquals(expectedValue,result);
-		
+	public void shouldFactorialOfTwoReturnTwo() {
+
+		long result = factorial.compute(2);
+		long expectedValue = 2;
+
+		assertEquals(expectedValue, result);
+
+	}
+
+	@Test
+	public void shouldFactorialOfFiveReturn120() {
+		long result = factorial.compute(5);
+		long expectedValue = 120;
+
+		assertEquals(expectedValue, result);
+
 	}
 }
