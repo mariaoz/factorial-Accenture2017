7a96008f0c4bb20e837d2933fbf0bb5dc108d0df
maria <maria@example.com>
2017-10-31 20:06:01 +0100

    Last Version

--- src/main/java/org/accenture/fac/Factorial.java
+++ src/main/java/org/accenture/fac/Factorial.java
@@ -6,14 +6,17 @@ package org.accenture.fac;
 public class Factorial {
 
 	public long compute(long value) {
+		long result;
 		if(value<0) {
 			throw new RuntimeException("The value is negative "+value);
 		}
 		else if ((value == 0) || (value == 1)) {
-			return 1;
+			result= 1;
 		} else {
-			return value * compute(value - 1);
+			result= value * compute(value - 1);
 		}
+		return result;
+		//comprobar que  no haya overflow
 	}
 
 }
